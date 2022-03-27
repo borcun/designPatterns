7575e4ffa8e45b40876f6908ec47d551cc9d7f89
boo <borcunozkablan@gmail.com>
2022-03-27 21:10:07 +0300

    singleton example is added

---
+++ Singleton/Singleton/Database.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class Database
+    {
+        // private class instance
+        private static Database m_database = null;
+        private string m_name;
+
+        // private default constructor to prevent explicit calls
+        private Database()
+        {
+            m_name = "";
+        }
+
+        public string Name
+        {
+            set
+            {
+                m_name = value;
+            }
+
+            get
+            {
+                return m_name;
+            }
+        }
+
+        // public static function that gets class instance
+        public static Database GetInstance()
+        {
+            if (null == m_database)
+            {
+                m_database = new Database();
+            }
+
+            return m_database;
+        }
+    }
+}
---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Singleton
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Database database1 = Database.GetInstance();
+            Database database2 = Database.GetInstance();
+
+            database1.Name = "MSSQL";
+            // last name is persistent, because of static object
+            database2.Name = "MySQL";
+
+            // hash codes are same
+            Console.WriteLine("{0} hash code: {1}", database1.Name, database1.GetHashCode());
+            Console.WriteLine("{0} hash code: {1}", database2.Name, database2.GetHashCode());
+
+        }
+    }
+}
